bbe52b9273e79cea6654e57fb97e4f283a5869d3
Craig <craig@2050.lateral.co.za>
2014-03-25 04:28:22 -0400

    X-Origin permitted

--- src/serve_html.rb
+++ src/serve_html.rb
@@ -10,16 +10,21 @@ class ServeHTML < Sinatra::Base
   #puts "Decc2050Model version = #{Decc2050Model.version}"
   puts "Decc2050Version.control_a1 = #{ModelShim.new.control_a1}"
 
+  set :protection, :except => :frame_options
+  puts "Set frame_options off"
+
   if development?
 
     helpers Helper
     set :views, settings.root 
 
     get '*' do
+      headers({ 'X-Frame-Options' => '' }) #'ALLOW-FROM apps.facebook.com' })
       haml :'default.html'
     end
   else
     get '*' do 
+      headers({ 'X-Frame-Options' => '' }) #ALLOW-FROM apps.facebook.com' })
       send_file 'public/default.html'
     end
   end
