78a618b022a68360c6df1b45b8f4945a0760979a
craigmj <craig@lateral.co.za>
2014-09-06 19:29:48 +0200

    one pagers

--- src/serve_html.rb
+++ src/serve_html.rb
@@ -5,7 +5,6 @@ require 'decc_2050_model'
 
 # This has the methods needed to dynamically create the view
 class ServeHTML < Sinatra::Base
-  puts "Here we are in serve_html.rb"
   puts "Decc2050Model last modified date = #{Decc2050Model.last_modified_date}"
   #puts "Decc2050Model version = #{Decc2050Model.version}"
   puts "Decc2050Version.control_a1 = #{ModelShim.new.control_a1}"
